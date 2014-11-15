9ff59e092b8774b7e329552f4df97bca701f3b40
Caleb Meredith <calebmeredith8@gmail.com>
2014-11-14 20:10:54 -0500

    Hooray

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,2 +1,23 @@
-var request = getParameterByName( 'the-search' );
-$( '#debug #request' ).html( request );
+$( '#search-submit' ).click( doSearch );
+$( '#search-container' ).submit( function( event ) {
+    doSearch();
+    event.preventDefault();
+});
+
+function doSearch() {
+    var search = $( '#search-bar' ).val();
+    
+    $( '#debug .search' ).html( search );
+
+    stuffWebMD( search );
+}
+
+function stuffWebMD( search ) {
+    $.ajax({
+        url: "http://www.webmd.com/cancer/default.htm",
+        success: function( result ) {
+            result = $( result );
+            $( '#view-results' ).html( result.find( '.teaser_fmt' ) );
+        },
+    });
+}
