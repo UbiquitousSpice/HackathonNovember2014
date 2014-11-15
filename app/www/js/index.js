2cb5f6fd649bbc852b64da9931da9b18796979ba
Caleb Meredith <calebmeredith8@gmail.com>
2014-11-15 04:25:46 -0500

    update index.js

---
+++ app/www/js/index.js
@@ -0,0 +1,54 @@
+viewSearch();
+
+$( '#search-submit' ).click( viewResults );
+$( '#search-container' ).submit( function( event ) {
+    viewResults();
+    event.preventDefault();
+});
+
+$( '.btn-negative' ).click( viewDoctors );
+$( '#btn-search' ).click( viewSearch );
+
+function viewSearch() {
+    $( '#view-search' ).show();
+    $( '#view-doctors' ).hide();
+    $( '#view-results' ).hide();
+    $( '#navbar' ).hide();
+    $( 'body' ).addClass( 'nice' );
+}
+
+function viewDoctors() {
+    $( '#view-search' ).hide();
+    $( '#view-doctors' ).show();
+    $( '#view-results' ).hide();
+    $( '#navbar' ).show();
+    $( 'body' ).removeClass( 'nice' );
+
+    $( '#navbar .title' ).html( 'Doctors' );
+}
+
+function viewResults() {
+    var search = $( '#search-bar' ).val();
+
+    $( '#view-search' ).hide();
+    $( '#view-doctors' ).hide();
+    $( '#view-results' ).show();
+    $( '#navbar' ).show();
+    $( 'body' ).removeClass( 'nice' );
+
+    $( '#navbar .title' ).html( 'Search: <em>' + search + '</em>' );
+
+    sourceWebMD( search );
+    sourceHealthLine( search );
+}
+
+function filterInfo( info ) {
+    if( info != null && info.html().length < 100 ) {
+        info = null;
+    }
+    $( info ).find( 'a' ).each( function() {
+        $( this ).attr( 'target', '_system' );
+    });
+    $( info ).find( '.lh-read-more' ).hide();
+    return info;
+}
