ad2003f9f5d3cfadf693d0e0ba28eafe27f14739
Caleb Meredith <calebmeredith8@gmail.com>
2014-11-14 23:58:01 -0500

    Adds iOS platform

---
+++ app/platforms/ios/www/js/index.js
@@ -0,0 +1,30 @@
+viewSearch();
+
+$( '#search-submit' ).click( viewResults );
+$( '#search-container' ).submit( function( event ) {
+    viewResults();
+    event.preventDefault();
+});
+
+$( '#btn-search' ).click( viewSearch );
+
+function viewResults() {
+    var search = $( '#search-bar' ).val();
+    
+    $( '#view-search' ).hide();
+    $( '#view-results' ).show();
+    $( '#navbar' ).show();
+    $( 'body' ).removeClass( 'nice' );
+
+    $( '#navbar .title' ).html( search );
+
+    stuffWebMD( search );
+    stuffWikipedia( search );
+}
+
+function viewSearch() {
+	$( '#view-search' ).show();
+    $( '#view-results' ).hide();
+    $( '#navbar' ).hide();
+    $( 'body' ).addClass( 'nice' );
+}
---
+++ app/platforms/ios/www/js/sources/webmd.js
@@ -0,0 +1,30 @@
+function stuffWebMD( search ) {
+    $( '#result-webmd' ).hide();
+    searchURL = 'http://www.webmd.com/search/search_results/default.aspx?query=' + encodeURI( search );
+    $.ajax({
+        url: searchURL,
+        success: function( result ) {
+            result = $( result );
+            searchResult = result.find( '#searchResults .spotlight_results .text_fmt h2 a' ).attr( 'href' );
+            console.log( 'WebMD Search Result: ' + searchResult );
+            $.ajax({
+                url: searchResult,
+                success: function( result ) {
+                    result = $( result );
+                    relevantInfo = result.find( '#mainContent_area p' ).html();
+                    if( relevantInfo != null && relevantInfo.length < 100 ) {
+                        relevantInfo = null;
+                    }
+                    if( relevantInfo == null ) {
+                        $( '#result-webmd' ).hide();
+                    } else {
+                        $( '#result-webmd' ).show();
+                        $( '#result-webmd .result-p' ).html( relevantInfo );
+                        $( '#result-webmd .result-a' ).attr( 'href', searchResult );
+                    }
+                },
+            });
+        },
+    });
+    return null;
+}
